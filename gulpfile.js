c92bc64e8f630306452e218ddb0dbaf8ded07d48
Daniel Fernando Lourusso <dflourusso@gmail.com>
2018-07-20 15:49:51 -0300

    async to promises

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ const babel = require('gulp-babel')
 gulp.task('default', () =>
   gulp
     .src('index.js')
-    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(
+      babel({
+        presets: ['es2015'],
+        plugins: ['async-to-promises']
+      })
+    )
     .pipe(gulp.dest('dist'))
 )
